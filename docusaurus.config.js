36eaf2e5445ee31343073dd71df685f3aa40593f
romer8 <gio.rombus@gmail.com>
2026-01-27 17:04:11 -0700

    fix: correct URL case for Courses page in configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -445,7 +445,7 @@ const config = {
                 label: "Software",
               },
              {
-                href: "/Courses",
+                href: "/courses",
                 label: "Courses",
               } 
             ],
